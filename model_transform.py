fix bugs relating with wrong rotation of no mirror wb

model_transform.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 def raw_pitch_transform_3d_points(pc_woodblock_points, pc_surface_points, pc_floor_points, np_border_points, matrix_z, mirror=False):
     surface_coeffs = get_surface_equation_coeffs(np.asarray(pc_floor_points.vertices), order=1)
-    transform_matrix = get_3d_transform_matrix(surface_coeffs, np_border_points, matrix_z)
+    transform_matrix = get_3d_transform_matrix(surface_coeffs, np_border_points, matrix_z, mirror)
     pc_woodblock_points.vertices = o3d.utility.Vector3dVector(transform_points(np.asarray(pc_woodblock_points.vertices),
                                                                                transform_matrix,
                                                                                mirror))

model_transform_utils.py
@@ -68,7 +68,7 @@ def _transform_homo_points(np_points, transform_matrix):
     return homo_np_3d
 
 
-def get_3d_transform_matrix(surface_coeffs, border_point=None, matrix_z=None):
+def get_3d_transform_matrix(surface_coeffs, border_point=None, matrix_z=None, mirror=False):
     """
     Get the translation matrix such that the 3D object can "lie" on Oxy (the surface of 3D object is Oxy).
     If border_point not None, rotate 3D object such that upper width vector is same direction as vector (1, 0)
@@ -92,6 +92,8 @@ def get_3d_transform_matrix(surface_coeffs, border_point=None, matrix_z=None):
     if border_point is not None:
         border_point = _transform_homo_points(border_point, matrix_z)
         border_point[:, 2] = 0
+        if not mirror:
+            border_point[:, 0] = - border_point[:, 0]
         yaw_matrix = get_yaw_rot_matrix(border_point)
         transform_matrix = np.matmul(yaw_matrix, transform_matrix)
     return transform_matrix

parsing.py
@@ -35,13 +35,16 @@ def get_point_from_via_file(via_json_path, keyword='whole'):
     return [np_src_point_list, np_dest_point_list]
 
 
-def get_border_points_from_via_file(via_json_path, key_src='depth_z'):
+def get_border_points_from_via_file(via_json_path, mirror=False, key_src='depth_z'):
     anno = read_json_file(via_json_path)
     depth_point_list = []
     for anno_key in anno.keys():
         if key_src in anno_key:
             for point_info in anno[anno_key]['regions']:
                 depth_point_list.append([point_info['shape_attributes']['cx'], point_info['shape_attributes']['cy']])
+    if not mirror:
+        border_points = depth_point_list[:2]
+        depth_point_list = [border_points[1], border_points[0]]
     np_depth_point_list = np.asarray(depth_point_list)
     return np_depth_point_list
 

process_pitching_yaw_transform.py
@@ -20,7 +20,7 @@ def process_pitching_yaw_transform(woodblock_path, floor_path, surface_path, bor
             pc_surface_points = read_stl_file(surface_path)
             pc_woodblock_points = read_stl_file(woodblock_path)
             matrix_z = np.load(str(matrix_z_path))
-            border_points = get_border_points_from_via_file(border_path)
+            border_points = get_border_points_from_via_file(border_path, mirror)
             border_points = np.hstack((border_points, np.zeros((border_points.shape[0], 1))))
             pc_woodblock_points, pc_surface_points = raw_pitch_transform_3d_points(pc_woodblock_points,
                                                                                    pc_surface_points,